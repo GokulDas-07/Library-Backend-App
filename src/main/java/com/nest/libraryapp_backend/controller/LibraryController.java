56dd2f7778ad5f41de0146ce712366f6bf40c949
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-05 23:04:43 +0530

    entry

---
+++ src/main/java/com/nest/libraryapp_backend/controller/LibraryController.java
@@ -0,0 +1,21 @@
+package com.nest.libraryapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibraryController {
+
+    @GetMapping("/")
+    public String HomePage()
+    {
+        return "Welcome to library home page";
+    }
+
+    @PostMapping("/entry")
+    public String BookEntry()
+    {
+        return "Book entry page";
+    }
+}
